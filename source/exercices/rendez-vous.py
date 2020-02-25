9d25a1e8ae6cc88f150f50cdd532ad4f3526e66c
GCleuziou <guillaume.cleuziou@gmail.com>
2020-02-25 21:13:02 +1100

    TP1:polynome+rendez-vous

---
+++ source/exercices/rendez-vous.py
@@ -0,0 +1,18 @@
+entrees_visibles = [
+        (1,10,7,20),
+        (1,7,10,20)
+]
+entrees_invisibles = [
+        (7,20,1,10),
+        (10,20,1,7),
+        (1,10,10,20),
+		(1,10,1,10)
+]
+
+@solution
+def rendezVous(d1,f1,d2,f2):
+   if f1<d2 or f2<d1:
+	   res False
+   else:
+	   res True
+   return res
