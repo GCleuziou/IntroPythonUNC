59ebc2a5398688c75fe58e4f7626268cb1bf2e24
GCleuziou <guillaume.cleuziou@gmail.com>
2020-04-09 21:47:31 +1100

    MAJ Résultats (9/04/20 pm)

--- source/tableau_defi/tableau_modele.py
+++ source/tableau_defi/tableau_modele.py
@@ -3,7 +3,7 @@
 
 nomFic='/Users/cleuziou/donnees/enseignement/UNC/IntroPython/easySphinx/source/tableau_defi/tentatives_csv.csv'
 exercices=["produitScalaire","motPalindrome","compteChiffre","phrasePalindrome","sousChaine","elemAri","elemGeo","verifSuiteAriGeo","suiteAri","suiteGeo","suiteAriGeo","nextConway"]
-idExercices=["4c4b7a924be986e72228374530812bf0","e2e71241db6a4971b7f407789330fdfd","908defc05cefcce7d5707781c7f9e500","3588d5b2366ad1ff5f1d78b184175afa","a7cdd094daa2f0bfccc7a4ec419f7243","f239498950e974e29e01472749d9a09d","d7c83a7fb9b49e28804e55955ad06968","415838ff0b81e52cfa661e514d52f26e","24fcf43c6eaccdd09abf09cc65703741","1bf7c612843eda9ce08f308b85561049","4c9665daf155b115fe13218141fa1aff","a580c32690609356dae4401b3c89a3b8"]
+idExercices=["0787173acdaeb235af8b8904fce81952","4c4b7a924be986e72228374530812bf0","e2e71241db6a4971b7f407789330fdfd","908defc05cefcce7d5707781c7f9e500","3588d5b2366ad1ff5f1d78b184175afa","a7cdd094daa2f0bfccc7a4ec419f7243","f239498950e974e29e01472749d9a09d","d7c83a7fb9b49e28804e55955ad06968","415838ff0b81e52cfa661e514d52f26e","24fcf43c6eaccdd09abf09cc65703741","1bf7c612843eda9ce08f308b85561049","4c9665daf155b115fe13218141fa1aff","a580c32690609356dae4401b3c89a3b8"]
 enseignants=['guillaume.cleuziou']
 
 def chargerResultats(nomFic,listeExercices,idExercices):
@@ -21,7 +21,10 @@ def chargerResultats(nomFic,listeExercices,idExercices):
 				decomp[0]+=elem[0].upper()+elem[1:]+' '
 			if decomp[0] not in resultats.keys():
 				resultats[decomp[0]]=dict()
-			resultats[decomp[0]][decomp[6]]=decomp[3]
+			if decomp[6] in resultats[decomp[0]].keys() and decomp[3]=='non':
+				pass
+			else :
+				resultats[decomp[0]][decomp[6]]=decomp[3]
 	fic.close()
 	return resultats
 
